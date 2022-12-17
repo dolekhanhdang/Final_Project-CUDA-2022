65af3e0794e200c364f54e896705112ed866bc7d
thanggnguyenn <102422555+thanggnguyenn@users.noreply.github.com>
2022-12-17 16:05:48 +0700

    add sample file

---
+++ SeamCarving_1.cu
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#define CHECK(call)\
+{\
+    const cudaError_t error = call;\
+    if (error != cudaSuccess)\
+    {\
+        fprintf(stderr, "Error: %s:%d, ", __FILE__, __LINE__);\
+        fprintf(stderr, "code: %d, reason: %s\n", error,\
+                cudaGetErrorString(error));\
+        exit(1);\
+    }\
+}
+
+struct GpuTimer
+{
+    cudaEvent_t start;
+    cudaEvent_t stop;
+
+    GpuTimer()
+    {
+        cudaEventCreate(&start);
+        cudaEventCreate(&stop);
+    }
+
+    ~GpuTimer()
+    {
+        cudaEventDestroy(start);
+        cudaEventDestroy(stop);
+    }
+
+    void Start()
+    {
+        cudaEventRecord(start, 0);
+        cudaEventSynchronize(start);
+    }
+
+    void Stop()
+    {
+        cudaEventRecord(stop, 0);
+    }
+
+    float Elapsed()
+    {
+        float elapsed;
+        cudaEventSynchronize(stop);
+        cudaEventElapsedTime(&elapsed, start, stop);
+        return elapsed;
+    }
+};
+
+void printDeviceInfo()
+{
+    cudaDeviceProp devProv;
+    CHECK(cudaGetDeviceProperties(&devProv, 0));
+    printf("**********GPU info**********\n");
+    printf("Name: %s\n", devProv.name);
+    printf("Compute capability: %d.%d\n", devProv.major, devProv.minor);
+    printf("Num SMs: %d\n", devProv.multiProcessorCount);
+    printf("Max num threads per SM: %d\n", devProv.maxThreadsPerMultiProcessor); 
+    printf("Max num warps per SM: %d\n", devProv.maxThreadsPerMultiProcessor / devProv.warpSize);
+    printf("GMEM: %zu byte\n", devProv.totalGlobalMem);
+    printf("SMEM per SM: %zu byte\n", devProv.sharedMemPerMultiprocessor);
+    printf("SMEM per block: %zu byte\n", devProv.sharedMemPerBlock);
+    printf("****************************\n");
+}
+
+int main(int argc, char ** argv)
+{
+    // PRINT OUT DEVICE INFO
+    printDeviceInfo();
+
+    return EXIT_SUCCESS;
+}
