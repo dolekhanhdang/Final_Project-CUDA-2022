20ef9bd2754ef5696878f32f121a29d8003c7157
dolekhanhdang <71361533+dolekhanhdang@users.noreply.github.com>
2022-12-25 13:48:51 +0700

    SeamCarving-Sequence-Dummy

--- SeamCarving_1.cu
+++ SeamCarving_1.cu
@@ -1,8 +1,9 @@
 ﻿#include <stdio.h>
 #include <stdint.h>
 #include <malloc.h>
+#include <math.h>
 
-
+#define NUM_REMOVE = 1000;
 #define CHECK(call)\
 {\
     const cudaError_t error = call;\
@@ -107,6 +108,7 @@ void writePnm(uchar3* pixels, int width, int height, char* fileName)
     fclose(f);
 }
 
+
 char* concatStr(const char* s1, const char* s2)
 {
     char* result = (char*)malloc(strlen(s1) + strlen(s2) + 1);
@@ -115,13 +117,14 @@ char* concatStr(const char* s1, const char* s2)
     return result;
 }
 
-void convolution(uchar3* inPixels, int width, int height, float* filter_x_Sobel, float* filter_y_Sobel, int filterWidth, uchar3* outPixels)
+void convolution(uchar3* inPixels, int width, int height, float* filter_x_Sobel, float* filter_y_Sobel, int filterWidth, uint8_t* outPixels)
 {
+    printf("    Convolution begin \n");
     for (int outPixelsR = 0; outPixelsR < height; outPixelsR++)
     {
         for (int outPixelsC = 0; outPixelsC < width; outPixelsC++)
         {
-            float3 outPixel = make_float3(0, 0, 0);
+            uint8_t outPixel = 0;
             for (int filterR = 0; filterR < filterWidth; filterR++)
             {
                 for (int filterC = 0; filterC < filterWidth; filterC++)
@@ -133,15 +136,139 @@ void convolution(uchar3* inPixels, int width, int height, float* filter_x_Sobel,
                     inPixelsR = min(max(0, inPixelsR), height - 1);
                     inPixelsC = min(max(0, inPixelsC), width - 1);
                     uchar3 inPixel = inPixels[inPixelsR * width + inPixelsC];
-                    outPixel.x += (filterVal_x + filterVal_y) * inPixel.x;
-                    outPixel.y += (filterVal_x + filterVal_y) * inPixel.y;
-                    outPixel.z += (filterVal_x + filterVal_y) * inPixel.z;
-                    float test = inPixel.x;
-                    if (outPixelsR * 512 + outPixelsC == -1)
-                        printf("%d - %f - x = %f - y = %f - z = %f \n", filterR * filterWidth + filterC, test, outPixel.x, outPixel.y, outPixel.z);
+                    uint8_t grayFilter = 0.299f * inPixel.x + 0.587f * inPixel.y + 0.114f * inPixel.z;
+                    outPixel += abs(filterVal_x * grayFilter) + abs(filterVal_y* grayFilter);
+
                 }
             }
-            outPixels[outPixelsR * width + outPixelsC] = make_uchar3(outPixel.x, outPixel.y, outPixel.z);
+            outPixels[outPixelsR * width + outPixelsC] = outPixel;
+        }
+    }
+}
+
+void seamTraceBack(int* traceBack, int* Sums, int* Seam, int width, int height)
+{
+    if ((traceBack == NULL) || (Sums == NULL) || (Seam == NULL))
+        return;
+    int Min = Sums[0];
+    int indexMin = 0;
+    for (int i = 0;i < width;i++)
+    {
+        int check = Sums[i];
+        if (check < Min)
+        {
+            Min = check;
+            indexMin = i;
+        }
+    }
+    printf("    indexMin = %d - min = %d \n", indexMin, Min);
+    Seam[0] = indexMin;
+    for (int i = 1;i < height;i++)
+    {
+        Seam[i] = traceBack[(i - 1) * width + indexMin];
+        indexMin = Seam[i];
+    }
+}
+
+void seamRemove(uchar3* inPixels, int* Seam, int width, int height, uchar3* newInPixels)
+{
+    for (int row = 0;row < height;row++)
+    {
+        int avoid = Seam[row];
+        for (int col = 0; col < width; col++)
+        {
+            if (col == avoid)
+                continue;
+            if (col < avoid)
+            {
+                newInPixels[row * (width - 1) + col] = inPixels[row * width + col];
+            }
+            else
+            {
+                newInPixels[row * (width - 1) + col-1] = inPixels[row * width + col];
+            }
+        }
+    }
+}
+
+void seamCal(uint8_t* inPixels, int width, int height, int* traceBack,int* Sums)
+{
+    if (inPixels == NULL)
+        return;
+    for (int row = height - 1; row >= 0; row--)
+    {
+        for (int col = 0; col < width; col++)
+        {
+            if (row == (height - 1))
+            {
+                traceBack[row * width + col] = INT_MAX;
+                Sums     [row * width + col] = inPixels[row * width + col];
+            }
+            else
+            {
+                int inPixel  = inPixels[row * width + col];
+                int minIndex = col;
+                int minValue = inPixel + Sums[(row + 1) * width + minIndex];
+                if (col - 1 >= 0)
+                {
+                    int checkValue = inPixel + Sums[(row + 1) * width + (col-1)];
+                    if (checkValue < minValue)
+                    {
+                        minIndex = col - 1;
+                        minValue = checkValue;
+                    }
+                }
+                if (col + 1 < width)
+                {
+                    int checkValue = inPixel + Sums[(row + 1) * width + (col + 1)];
+                    if (checkValue < minValue)
+                    {
+                        minIndex = col + 1;
+                        minValue = checkValue;
+                    }
+                }
+                traceBack[row * width + col] = minIndex;
+                Sums     [row * width + col] = minValue;
+            }
+        }
+    }
+}
+
+void seamCarving(uchar3* inPixels, int width, int height, uchar3*& outPixels, int numColRemove)
+{
+    for (int i = 1;i <= numColRemove; i++)
+    {
+        printf("Seam = %d \n", i);
+        //Define varivable
+        int  new_width     = width - i + 1;
+        int* traceBack    = (int*)   malloc(new_width * height * sizeof(int));
+        int* Sums         = (int*)   malloc(new_width * height * sizeof(int));
+        int* Seam         = (int*)   malloc(            height * sizeof(int));
+        uchar3* newPixels = (uchar3*)malloc((new_width -1) * height * sizeof(uchar3));
+        // Define filter
+        int filterWidth = 3;
+        float filter_x_Sobel[9] = { 1 , 0 , −1, 2 , 0 , −2,  1 ,  0 , −1 };
+        float filter_y_Sobel[9] = { 1 , 2 , 1 , 0 , 0 , 0 , −1 , −2 , −1 };
+        // Gray filter and convolution ( calculate Importance matrix )
+        uint8_t* gray   = (uint8_t*)malloc(new_width * height * sizeof(uint8_t));
+        convolution       (inPixels , new_width, height, filter_x_Sobel, filter_y_Sobel, filterWidth, gray);
+        seamCal           (gray     , new_width, height,traceBack,Sums);
+        seamTraceBack     (traceBack, Sums, Seam, new_width, height);
+        seamRemove        (inPixels , Seam, new_width, height, newPixels);
+        uchar3* temp    = inPixels;
+        inPixels        = newPixels;
+        newPixels       = temp;
+        if (i > 1)
+            free(newPixels);
+        free(traceBack);
+        free(Sums);
+        free(Seam);
+        free(gray);
+        if (i == numColRemove)
+        {
+            outPixels = inPixels;
+            printf("DONE ! \n");
+            return;
         }
     }
 }
@@ -180,18 +307,21 @@ int main(int argc, char ** argv)
     printf("\nImage size (width x height): %i x %i\n", width, height);
 
     // Calculation
-    int filterWidth = 3;
-    float filter_x_Sobel[9] = { 1 ,0, −1,2, 0, −2,1, 0, −1};
-    float filter_y_Sobel[9] = { 1, 2, 1, 0, 0, 0, −1, −2, −1};
-    uchar3* outPixels = (uchar3*)malloc(width * height * sizeof(uchar3));
-    convolution(inPixels,width,height,filter_x_Sobel,filter_y_Sobel,filterWidth, outPixels);
-
+    uchar3* outPixels = NULL;
+    GpuTimer timer;
+    timer.Start();
+    seamCarving(inPixels, width, height, outPixels, 1000);
+    //uint8_t testSeam[15] = { 1, 4, 3, 5, 2, 3, 2, 5, 2, 3, 5, 3, 4, 2, 1}; // Only for test purpose
+    timer.Stop();
+    printf("Time: %.3f ms\n", timer.Elapsed());
     // Write results to files
     char* outFileNameBase = strtok(argv[2], "."); // Get rid of extension
     //writePnm(correctOutPixels, 1, width, height, concatStr(outFileNameBase, "_host.pnm"));
-    writePnm(outPixels, width, height, concatStr(outFileNameBase, "_device.pnm"));
-
+    if(outPixels != NULL)
+        writePnm(outPixels, width-1000, height, concatStr(outFileNameBase, "_device.pnm"));
+    printf("HMM \n");
     // Free memories
-    free(inPixels);
+    free(inPixels );
     free(outPixels);
 }
+
